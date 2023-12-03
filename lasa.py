8072cf82637d79b30f0a0b18c8c84f412bf97797
omagebright <omagefolorunsho@gmail.com>
2023-12-03 17:23:11 -0300

    Modify the LASA file for calculating the lost of accessibility surface area per residue, presenting the result in a more refined manner

--- lasa.py
+++ lasa.py
@@ -1,52 +1,40 @@
-# This script subtract per-residue area, complex from apo
-
 import os
+import pandas as pd
 
-# Define the directories
+# Define directories
 protein_dir = 'allosteric_proteins'
 complex_dir = 'protein-modulator_complexes'
-lasa_dir = 'LASA'
+output_dir = 'complete_LASA'
 
-# Create LASA directory if it doesn't exist
-if not os.path.exists(lasa_dir):
-    os.makedirs(lasa_dir)
-
-# Get the list of protein files
+# Get list of protein files
 protein_files = [f for f in os.listdir(protein_dir) if f.endswith('.res')]
 
 for protein_file in protein_files:
-    # Open the protein file and read the ASA values
-    with open(os.path.join(protein_dir, protein_file), 'r') as f:
-        protein_asa = []
-        for line in f:
-            if line.strip():
-                try:
-                    asa_value = float(line.split()[-1])
-                    protein_asa.append(asa_value)
-                except ValueError:
-                    continue
-
-    # Open the corresponding complex file and read the ASA values
+    # Construct complex file name
     complex_file = protein_file.replace('.res', '_complex.res')
+
+    # Check if complex file exists and is not empty
     complex_path = os.path.join(complex_dir, complex_file)
-    if not os.path.isfile(complex_path):
-        print(f"Skipping {complex_file} as it does not exist.")
+    if not os.path.exists(complex_path) or os.stat(complex_path).st_size == 0:
+        print(f'Skipping file: {complex_file}')
         continue
 
-    with open(complex_path, 'r') as f:
-        complex_asa = []
-        for line in f:
-            if line.strip():
-                try:
-                    asa_value = float(line.split()[-1])
-                    complex_asa.append(asa_value)
-                except ValueError:
-                    continue
+    # Read protein file
+    protein_df = pd.read_csv(os.path.join(protein_dir, protein_file), delim_whitespace=True, skiprows=1, skipfooter=1, engine='python', names=['residue_name', 'chain_name', 'number', 'protein_area'])
 
-    # Calculate the LASA values
-    lasa = [p - c for p, c in zip(protein_asa, complex_asa)]
+    # Read complex file
+    complex_df = pd.read_csv(complex_path, delim_whitespace=True, skiprows=1, skipfooter=1, engine='python')
+    complex_area = complex_df.iloc[:, -1]  # Get last column
 
-    # Write the LASA values to a new file in the LASA directory
-    with open(os.path.join(lasa_dir, protein_file.replace('.res', '.txt')), 'w') as f:
-        for value in lasa:
-            f.write(str(value) + '\n')
+    # Shift complex_area by one row
+    complex_area = complex_area.shift(1)
+
+    # Add complex_area to protein_df
+    protein_df['complex_area'] = complex_area
+
+    # Calculate LASA
+    protein_df['LASA'] = protein_df['protein_area'] - complex_area
+
+    # Write output to file
+    output_file = protein_file.replace('.res', '.txt')
+    protein_df.to_csv(os.path.join(output_dir, output_file), sep='\t', index=False)
